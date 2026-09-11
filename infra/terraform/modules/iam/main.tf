b3afd1a4ae5f09765597ac8e9294467f87a2f4ff
BGYKanishka <bgykanishka@gmail.com>
2026-09-11 19:35:35 +0530

    Fix GitHub OIDC provider thumbprint

--- infra/terraform/modules/iam/main.tf
+++ infra/terraform/modules/iam/main.tf
@@ -2,7 +2,7 @@
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612", "6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
 }
 
 # Create IAM Role that GitHub Actions can assume
